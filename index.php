a283fd1ae8593d54a8a81b076989d3526643482c
Colin Kiama <colinkiama@gmail.com>
2025-04-03 00:18:58 +0100

    Start adding home page content and apply basic styling

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>4MB Game Jam</title>
+        <link rel="stylesheet" href="assets/styles/index.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </head>
+
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">About Us</a>
+                    </li>
+
+                    <li>
+                        <a href="#">Rules</a>
+                    </li>
+
+                    <li>
+                        <a href="#">Resources</a>
+                    </li>
+
+                    <li>
+                        <a href="#">Community</a>
+                    </li>
+
+                    <li>
+                        <a href="#">Archive</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <img src="assets/img/2025-logo.png" width="255" height="215" alt="4MB Jam 2025 Logo">
+            <h1>4MB Game Jam 2025</h1>
+            <p>Create a game that fits wihtin 4MB!</p>
+            <a href="#">Register</a>
+            <a href="#">Learn More</a>
+        </header>
+    </body>
+</html>
