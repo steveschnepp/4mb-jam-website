1245e41fde0d789123dbbcd6c9604524a7c919c2
Colin Kiama <colinkiama@gmail.com>
2025-04-09 19:33:47 +0100

    Community links are now in a 4 column grid

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
                     [
                         'id' => 'community',
                         'content' => <<< 'HTMLQUOTE'
-                <ul>
+                <ul class="community-grid">
                     <li><a href="https://discord.gg/ge3YuJheBY">Discord</a></li>
                     <li><a href="https://mastodon.social/@4mbjam">Mastodon</a></li>
                 </ul>
